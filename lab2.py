02fe2063b391804ee1a453b534f47a19438fba8f
hadrianpaulo <hadrianpaulo@gmail.com>
2016-03-12 09:19:49 +0800

    Added Sobel filtering and ROI features extraction

--- lab2.py
+++ lab2.py
@@ -27,19 +27,27 @@ def auto_canny(image, sigma=0.33):
     return edged
 
 image = cv2.imread("balloons.jpg")
-blurred = cv2.GaussianBlur(image, (5, 5), 0)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)  # Common use case of 5x5
 edges = auto_canny(blurred)
 showImage('Canny', edges)
 
 contoured_image, contours, hierarchy = cv2.findContours(
     edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-ROI = pd.DataFrame(columns=['y', 'y1', 'x', 'x1'])
+# ROI -> Regions of Interest; bb: bounding box
+ROI_bb = pd.DataFrame(columns=['y', 'y1', 'x', 'x1'])
+# Will contain 8x8 feature vectors for BING
+ROI_features = pd.DataFrame(columns=['feature_' + str(x) for x in range(64)])
 counter = 0
 image_out = image.copy()
-# Draw rectangles enclosing contours in orig image
+
+# ddepth for sobel
+ddepth = cv2.CV_8U
+
+# Draw bounding boxes enclosing contours in orig image
 for contour in contours:
-    # get rectangle bounding contour
+    # get box bounding contour
     [x, y, w, h] = cv2.boundingRect(contour)
 
     # discard areas that are too large
@@ -50,14 +58,30 @@ for contour in contours:
     if h < image.shape[1] * 0.05 or w < image.shape[0] * 0.05:
         continue
 
-    # draw rectangle around contour on original image
+    # crop ROI, store bounding box info, store img mat, save img
     crop_img = image[y:y + h, x:x + w]
-    ROI.loc[counter] = [y, y + h, x, x + h]
+    crop_img_gray = gray[y:y + h, x:x + w]
+    ROI_bb.loc[counter] = [y, y + h, x, x + h]
     cv2.imwrite('ROI/' + str(counter) + '.jpg', crop_img)
+
+    # Doing BING -> http://mmcheng.net/bing/
+    # Do Sobel for x and y (set dx=1, dy=0, viceversa)
+    sobel_x = cv2.Sobel(crop_img_gray, ddepth, 1, 0)
+    sobel_y = cv2.Sobel(crop_img_gray, ddepth, 0, 1)
+    # merge sobel dx and dy images via cv2.addWeight with 0.5 alpha/beta,
+    # 0 gamma
+    sobel = cv2.addWeighted(sobel_x, 0.5, sobel_y, 0.5, 0)
+    # Resize to 8x8 image, flatten, and store to ROI_features
+    sobel_flat = cv2.resize(sobel, (8, 8)).flatten()
+    ROI_features.loc[counter] = sobel_flat
+    # draw a box around contour on original image
     cv2.rectangle(image_out, (x, y), (x + w, y + h), (255, 0, 255), 2)
     counter += 1
-showImage('Bounded Rectangles', image_out)
-ROI.to_csv('ROI_coords.csv', index=False)
 
+showImage('Bounded Rectangles', image_out)
+ROI_bb.to_csv('ROI_coords.csv', index=False)
+ROI_features.to_csv('ROI_features.csv', index=False)
 # have not implemented 1 e and f though i think not needed
 # C++ specifics kasi
+
+# Naive Bayes to follow
