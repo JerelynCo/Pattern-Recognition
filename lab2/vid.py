c8871b54b7d92bb139e671352c7febd36c82bf70
hadrianpaulo <hadrianpaulo@gmail.com>
2016-03-17 15:48:40 +0800

    Updated vid.py with naive bayes' prediction functions

--- lab2/vid.py
+++ lab2/vid.py
@@ -1,5 +1,33 @@
 import numpy as np
 import cv2
+import math
+
+
+def calculateProbability(x, mean, stdev):
+    print("Probability returned.")
+    exponent = math.exp(-(math.pow(x - mean, 2) / (2 * math.pow(stdev, 2))))
+    return (1 / (math.sqrt(2 * math.pi) * stdev)) * exponent
+
+
+def calculateClassProbabilities(summaries, inputVector):
+    probabilities = {}
+    for classValue, classSummaries in summaries.items():
+        probabilities[classValue] = 1
+        for i in range(len(classSummaries)):
+            mean, stdev = classSummaries[i]
+            x = inputVector[i]
+            probabilities[classValue] *= calculateProbability(x, mean, stdev)
+    return probabilities
+
+
+def predict(summaries, inputVector):
+    probabilities = calculateClassProbabilities(summaries, inputVector)
+    bestLabel, bestProb = None, -1
+    for classValue, probability in probabilities.items():
+        if bestLabel is None or probability > bestProb:
+            bestProb = probability
+            bestLabel = classValue
+    return bestLabel
 
 
 def auto_canny(image, sigma=0.33):
