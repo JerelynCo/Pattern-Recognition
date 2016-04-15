32da5b89fa7cb4797e924a32a5a91e8d3a714bd4
hadrianpaulo <hadrianpaulo@gmail.com>
2016-04-15 08:04:26 +0800

    Added storage of zeta per layer on feedforward process. Renamed compute_cost to fit pep8 Added safeguards for network initialization with predefined weights

--- pynn/network.py
+++ pynn/network.py
@@ -4,25 +4,37 @@ import numpy as np
 class Network:
     # Example topology is [3,5,1] with input having 3 neurons
     # hidden layer with 5, output wuth 1
-    def __init__(self, topology, weights=[]):
+    def __init__(self, topology, weights=np.array([])):
         self.topology = topology
-        # [5 x 3], [1 x 5]
-        dims = list(zip(topology[1:], topology))
-        # weights[between what layers][neuron_i][neuron_j]
-        self.weights = np.array([np.random.random(i) for i in dims
-                                 ])
+        if (weights.size == 0):
+            # [5 x 3], [1 x 5]
+            dims = list(zip(topology[1:], topology))
+            # weights[between what layers][neuron_i][neuron_j]
+            self.weights = np.array([np.random.random(i) for i in dims
+                                     ])
+        else:
+            assert weights.size == len(topology) - 1, "weights size does not match topology"
+            self.weights = weights
+        # zeta values are the outputs values of a layer prior to being subjected
+        # to the activation function. This is used for backpropagation
+        # Input data is not part of zeta
+        self.zeta_layer = []
 
-    def activation(self, zeta):
+    @staticmethod
+    def activation(zeta):
         return np.tanh(zeta)
 
     def feedforward(self, input_data):
         assert input_data.size == self.topology[0], "input size exceeds number of input nodes"
-        zeta = input_data
+        self.zeta_layer = []
+        alpha = input_data
         for layer_weights in self.weights:
-            zeta = self.activation(np.dot(layer_weights, zeta))
-        return zeta
+            zeta = np.dot(layer_weights, alpha)
+            self.zeta_layer.append(zeta)
+            alpha = self.activation(zeta)
+        return alpha
 
-    def computeCost(self, zeta, target):
+    def compute_cost(self, zeta, target):
         assert len(target) == self.topology[-1], "target size exceeds number of output nodes."
         target = np.array(target)
         return np.sum(np.square(zeta - target))
--- pynn/test.py
+++ pynn/test.py
@@ -1,10 +1,12 @@
 import numpy as np
+
 from network import Network
 
-topology = [600, 10, 2]
-data = np.random.rand(600)
+topology = [5, 3, 2]
+data = np.random.rand(5)
 
 net = Network(topology)
 output = net.feedforward(data)
+print(net.zeta_layer)
+print(net.weights)
 print(output)
-print(net.computeCost(output, [0.2, -0.3]))
