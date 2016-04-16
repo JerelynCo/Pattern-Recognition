initial fix backprop

pynn/network.py
@@ -10,7 +10,7 @@ class Network:
         self.topology = topology
         if (weights.size == 0):
             # [5 x 3], [1 x 5]
-            dims = list(zip(topology[1:], topology))
+            dims = list(zip(topology[1:], topology[:-1]))
             # weights[between what layers][neuron_i][neuron_j]
             self.weights = np.array([np.random.random(i) for i in dims
                                      ])
@@ -18,48 +18,15 @@ class Network:
             assert weights.size == len(
                 topology) - 1, "weights size does not match topology"
             self.weights = weights
+
         # zeta values are the outputs values of a layer prior to being subjected
         # to the activation function. This is used for backpropagation
         # Input data is not part of zeta
         self.zeta_layer = []
 
-    def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
-        # Training data: list of tuples (x = inputs, y=target)
-        # Test data: if provided, network will be evaluated against it
-        for epoch in range(epochs):
-            random.shuffle(training_data)
-            mini_batches = [training_data[index:index+mini_batch_size] for index in range(0, len(test_data), mini_batch_size)]
-
-            for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta)
-
-            if test_data:
-                print("Epoch {0}: {1}/{2}".format(epoch, self.evaluate(test_data), len(test_data)))
-            
-            else:
-                print("Epoch {0} complete".format(epoch))
-
-    def update_mini_batch(self, mini_batch, eta):
-        # Updates weights by backprop per batch
-        for x, y in mini_batch:
-            nabla_w = [np.zeros(w.shape) for w in self.weights]
-            delta_nabla_w = self.backprop(x,y)
-            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-        self.weights = [w-(eta/len(mini_batch))*nw for w,nw in zip(self.weights,nabla_w)]
-
-    def evaluate(self, test_data):
-        # Classification: resolved by getting the highest output value's index
-        test_results = [(np.argmax(self.feedforward(x)), y)
-                        for (x, y) in test_data]
-        return sum(int(x == y) for (x, y) in test_results)
-
     @staticmethod
     def activation(zeta):
         return np.tanh(zeta)
-
-    @staticmethod
-    def activation_prime(zeta):
-        return np.square(np.tanh(zeta)) + 1
 
     @staticmethod
     def activation_prime(zeta):
@@ -69,35 +36,45 @@ class Network:
         assert input_data.size == self.topology[
             0], "input size exceeds number of input nodes"
         self.zeta_layers = []
-        self.activations_layer = []
+        self.activation_layers = [input_data]
         alpha = input_data
         for layer_weights in self.weights:
             zeta = np.dot(layer_weights, alpha)
             self.zeta_layers.append(zeta)
 
             alpha = self.activation(zeta)
-            self.activations_layer.append(alpha)
+            self.activation_layers.append(alpha)
         return alpha
+
+    def update_weights(self, input_data, target, eta):
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        delta_nabla_w = self.backprop(input_data, target)
+        nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [
+            w - (eta / len(input_data)) * nw for w, nw in zip(self.weights, nabla_w)]
 
     def backprop(self, input_data, target):
         nabla_w = [np.zeros(w.shape) for w in self.weights]
+
         delta = self.cost_derivative(
-            self.activations_layer[-1], target) * self.activation_prime(self.zeta_layers[-1])
-        nabla_w = np.dot(delta, self.activations_layer[-1].transpose())
+            self.activation_layers[-1], target) * self.activation_prime(
+            self.zeta_layers[-1])
+        nabla_w[-1] = np.dot(delta, np.transpose(self.activation_layers[-1]))
 
         for l in range(2, len(self.topology)):
             # From the second to the last layer backwards
-            delta = np.dot(
-                self.weights[-l + 1].transpose(), delta) * self.activation_prime(self.zeta_layers[-l])
-            nabla_w = np.dot(delta, self.activations_layer[-l].transpose())
+            delta = np.dot(np.transpose(
+                self.weights[-l + 1]), delta) * self.activation_prime(self.zeta_layers[-l])
+            nabla_w[-l] = np.dot(
+                delta, np.transpose(self.activation_layers[-l]))
         return nabla_w
 
-
-    def compute_cost(self, zeta, target):
+    def compute_cost(self, activation, target):
         assert len(
             target) == self.topology[-1], "target size exceeds number of output nodes."
         target = np.array(target)
-        return np.sum(np.square(zeta - target))
+        return np.sum(np.square(activation - target))
 
     def cost_derivative(self, output_activations, target):
         # Partial derivatives for the output activations

pynn/test.py
@@ -3,11 +3,18 @@ from network import Network
 
 topology = [5, 3, 2]
 
-data = np.random.rand(5)
+data = np.array([0.5, -0.23, 0.45, -0.123, 0.55])
 target = [1, 2]
+
+epochs = 5	
+eta = 0.15
 
 net = Network(topology)
 output = net.feedforward(data)
-print(net.zeta_layer)
-print(net.weights)
-print(output)
+
+for epoch in range(epochs):
+	output = net.feedforward(data)
+	net.update_weights(data, target, eta)
+	print("Epoch {0}: {1}".format(epoch, net.compute_cost(output, target)))
+	print("Weights: {0}".format(net.weights))
+