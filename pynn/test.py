d4e99ea569d6826cd4a4ac658f7ae8de7582f782
hadrianpaulo <hadrianpaulo@gmail.com>
2016-04-19 15:29:44 +0800

    with bias

--- pynn/test.py
+++ pynn/test.py
@@ -4,20 +4,25 @@ from network import Network
 
 topology = [5, 4, 2]
 
-data = np.array([0.5, -0.23, 0.45, -0.123, 0.55])
-target = [0, 1]
+data = np.array(np.random.random([2, 5]))
+
+target = [[0, 1],
+          [1, 0]]
 
 epochs = 500
-eta = 0.10
+eta = 0.5
 
 net = Network(topology)
-output = net.feedforward(data)
 
 for epoch in range(epochs):
-    output = net.feedforward(data)
-    net.backprop(eta, target)
+    output = net.feedforward(data[0])
+    net.backprop(eta, target[0])
+    output1 = net.feedforward(data[1])
+    net.backprop(eta, target[1])
     print("Epoch {0}: {1}".format(epoch, net.compute_cost(output, target)))
     print("Epoch {0} Output: {1} ".format(epoch, output))
+    print("Epoch {0}: {1}".format(epoch, net.compute_cost(output1, target)))
+    print("Epoch {0} Output: {1} ".format(epoch, output1))
     #
     # output = net.feedforward(data)
     # net.backprop(eta, target)
